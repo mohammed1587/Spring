fb3e61d2f23f8c3e2a858b4a71c619550fa8655d
MTouab <mohammed.touab@gmail.com>
2021-11-17 20:44:45 +0100

    Add Validators

---
+++ src/main/java/com/example/spring/validator/CategoryValidator.java
@@ -0,0 +1,20 @@
+package com.example.spring.validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.spring.dto.CategoryDto;
+import org.springframework.util.StringUtils;
+
+public class CategoryValidator {
+
+  public static List<String> validate(CategoryDto categoryDto) {
+    List<String> errors = new ArrayList<>();
+
+    if (categoryDto == null || !StringUtils.hasLength(categoryDto.getCode())) {
+      errors.add("Veuillez renseigner le code de la categorie");
+    }
+    return errors;
+  }
+
+}
