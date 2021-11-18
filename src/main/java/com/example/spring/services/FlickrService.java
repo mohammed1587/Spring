22a63dbc8f0eb37d9ad6caac7d6bb525eb646e1b
MTouab <mohammed.touab@gmail.com>
2021-11-18 21:49:53 +0100

    Config Flickr

--- src/main/java/com/example/spring/services/FlickrService.java
+++ src/main/java/com/example/spring/services/FlickrService.java
@@ -1,9 +1,11 @@
 package com.example.spring.services;
 
+ import com.flickr4java.flickr.FlickrException;
+
  import java.io.InputStream;
 
 public interface FlickrService {
 
-  String savePhoto(InputStream photo, String title);
+  String savePhoto(InputStream photo, String title) throws FlickrException;
 
 }
